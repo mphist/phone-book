86c80f1ea3f7158b9cd8d0c0d1dc93079e272d3b
Kevin Baik <mphist@gmail.com>
2018-10-21 15:47:24 -0400

    phone-book

--- src/App.js
+++ src/App.js
@@ -4,10 +4,30 @@ import './App.css';
 import PhoneForm from "./components/PhoneForm";
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: "김민준",
+        phone: "010-0000-0000"
+      },
+      {
+        id: 1,
+        name: "홍길동",
+        phone: "010-0000-0001"
+      }
+    ]
+  }
+
+  handleCreate = (data) => {
+    console.log(data)
+  }
+
   render() {
     return (
       <div className="App">
-        <PhoneForm />
+        <PhoneForm onCreate={this.handleCreate}/>
       </div>
     );
   }
